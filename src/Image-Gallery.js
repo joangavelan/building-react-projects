5bb2242a65fd8307e1b53785fcd76f17d5ee403b
Johan Gavelan <johangavelan@gmail.com>
2021-02-24 22:52:01 -0500

    Image Gallery - project finished

--- src/Image-Gallery.js
+++ src/Image-Gallery.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Image-Gallery.css'
 
+const images = [
+  {id: 1, name: 'biodiversity', src: 'images/biodiversity.jpg'},
+  {id: 2, name: 'outer-space', src: 'images/outer-space.jpg'},
+  {id: 3, name: 'rise-of-skywalker', src: 'images/rise-of-skywalker.jpg'},
+  {id: 4, name: 'world', src: 'images/world.jpg'},
+  {id: 5, name: 'island', src: 'images/island.jpg'},
+  {id: 6, name: 'earth', src: 'images/earth.jpg'}
+];
+
 const ImageGallery = () => {
+  const [selectedImage, setImage] = useState(images[0]);
+
   return (
     <div className="image-gallery">
-      <div>
-        <img src="images/biodiversidad.jpg" alt="biodiversity"/>
-        <img src="images/outer-space.jpg" alt="outer space"/>
-        <img src="images/rise-of-skywalker.jpg" alt="rise of skywalker"/>
-      </div>
-      <img className="selected-image" src="images/island.jpg" alt="biodiversity"/>
-      <div>
-        <img src="images/world.jpg" alt="world"/>
-        <img className="selected" src="images/island.jpg" alt="island"/>
-        <img src="images/earth.jpg" alt="earth"/>
+      <img className="selected-image" src={selectedImage.src} alt={selectedImage.name}/>
+      <div className="images">
+        {images.map(image => (
+          <img 
+            key={image.id} 
+            onClick={() => setImage(image)} 
+            src={image.src}
+            alt={image.name}
+            className={selectedImage === image ? 'selected' : null}/>
+        ))}
       </div>
     </div>
   )
 }
 
-export default ImageGallery
+export default ImageGallery 
